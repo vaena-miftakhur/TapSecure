ef5d6fdf656c0834e2bc81df5200fe4d54173931
vaena-miftakhur <vaenamiftakhurrisko@gmail.com>
2026-05-01 07:40:27 +0700

    koneksi database

--- src/main/java/com/mycompany/tapsecure/TapSecure.java
+++ src/main/java/com/mycompany/tapsecure/TapSecure.java
@@ -4,13 +4,20 @@
 
 package com.mycompany.tapsecure;
 
+import com.mycompany.tapsecure.objects.Karyawan;
+
 /**
  *
  * @author LENOVO
  */
 public class TapSecure {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Karyawan KR = new Karyawan();
+        if(KR instanceof Karyawan){
+            System.err.println("Karyawan");
+        }else {
+            System.err.println("Something else");
+        }// //
+        // //
     }
 }
---
+++ src/main/java/com/mycompany/tapsecure/objects/MongoManager.java
@@ -0,0 +1,31 @@
+package com.mycompany.tapsecure.objects;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+public class MongoManager {
+    private static MongoClient mongoClient;
+    private static final String DATABASE_NAME = "tapsecure_db";
+
+    public static MongoDatabase getDatabase() {
+        if (mongoClient == null) {
+            // Konfigurasi CodecRegistry untuk pemetaan POJO otomatis (Standard Industry)
+            CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
+                MongoClientSettings.getDefaultCodecRegistry(),
+                CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
+            );
+
+            // Inisiasi koneksi ke MongoDB Localhost (Driver 5.0.0)
+            mongoClient = MongoClients.create("mongodb://localhost:27017");
+            
+            // Mengembalikan database dengan registry yang sudah dikonfigurasi
+            return mongoClient.getDatabase(DATABASE_NAME).withCodecRegistry(pojoCodecRegistry);
+        }
+        return mongoClient.getDatabase(DATABASE_NAME);
+    }
+}
